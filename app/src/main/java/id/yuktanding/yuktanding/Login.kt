e515470c640c8a7c004fdec4dee226f54ae6c157
ekhiw <ekkyhiw@gmail.com>
2017-08-18 21:58:43 +0700

    Picasso ganti ke placeholder, tambah tombol login

--- app/src/main/java/id/yuktanding/yuktanding/Login.kt
+++ app/src/main/java/id/yuktanding/yuktanding/Login.kt
@@ -1,28 +1,36 @@
 package id.yuktanding.yuktanding
 
-import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.TextInputEditText
+import android.support.design.widget.TextInputLayout
 import android.util.Log
-import android.view.Window
 import android.widget.ImageView
+import android.widget.Toast
 import com.squareup.picasso.Picasso
 
 class Login : AppCompatActivity() {
     private var ib: ImageView?=null
-    private var uin: TextInputEditText?=null
+    private var uie: TextInputEditText?=null
+    private var uil: TextInputLayout?=null
     val logg= "Disini"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(logg,"onCreate")
         setContentView(R.layout.activity_login)
+        Log.d(logg,"setelah layout")
 
-        uin = findViewById(R.id.user_input_edit) as TextInputEditText
+        uil = findViewById(R.id.user_input_layout) as TextInputLayout
+        uie = findViewById(R.id.user_input_edit) as TextInputEditText
         ib = findViewById(R.id.image_background) as ImageView
 
-        Picasso.with(this).load("https://yuktanding.id/img/lap.jpg").into(ib)
-
+        Log.d(logg,"sebelum Picasso")
+        Picasso.with(this)
+                .load("https://yuktanding.id/img/lap.jpg")
+                .placeholder(R.drawable.lap1)
+                .error(R.drawable.lap1)
+                .into(ib);
+        Log.d(logg,"setelah Picasso")
     }
 }
