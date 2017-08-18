712606f7d19747dfc436661bd78b9af886b48387
ekhiw <ekkyhiw@gmail.com>
2017-08-18 15:58:32 +0700

    Ganti style

--- app/src/main/java/id/yuktanding/yuktanding/Login.kt
+++ app/src/main/java/id/yuktanding/yuktanding/Login.kt
@@ -1,7 +1,9 @@
 package id.yuktanding.yuktanding
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
 import android.util.Log
 import android.view.Window
 import android.widget.ImageView
@@ -9,15 +11,18 @@ import com.squareup.picasso.Picasso
 
 class Login : AppCompatActivity() {
     private var ib: ImageView?=null
-    public val logg= "Disini"
+    private var uin: TextInputEditText?=null
+    val logg= "Disini"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(logg,"onCreate")
         setContentView(R.layout.activity_login)
 
+        uin = findViewById(R.id.user_input_edit) as TextInputEditText
         ib = findViewById(R.id.image_background) as ImageView
 
         Picasso.with(this).load("https://yuktanding.id/img/lap.jpg").into(ib)
+
     }
 }
