a35c4fa235d763621387bb4a8fc73bd2fd2c3349
ekhiw <ekkyhiw@gmail.com>
2017-08-26 19:09:19 +0700

    Udah bisa ganti activity pake spannable string

--- app/src/main/java/id/yuktanding/yuktanding/Login.kt
+++ app/src/main/java/id/yuktanding/yuktanding/Login.kt
@@ -4,25 +4,35 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.TextInputEditText
 import android.support.design.widget.TextInputLayout
+import android.text.SpannableString
+import android.text.style.ClickableSpan
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import com.github.kittinunf.fuel.Fuel
 import com.squareup.picasso.Picasso
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.AuthResult
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
+import android.text.method.LinkMovementMethod
+import android.text.Spanned
+import android.text.TextPaint
+import android.content.Intent
+import android.view.View
 
 
 class Login : AppCompatActivity() {
     private var ib: ImageView?=null
+    private var link: TextView?=null
+    private var span: SpannableString?=null
     private var uie: TextInputEditText?=null
     private var uil: TextInputLayout?=null
     private var btnSignin: Button?=null
     private var btnSignGoogle: Button?=null
     private var mAuth: FirebaseAuth? = null
-    private val mAuthListener: FirebaseAuth.AuthStateListener? = null
+    //private val mAuthListener: FirebaseAuth.AuthStateListener? = null
     val TAG = "Disini"
 
     //==============================================================================================
@@ -31,30 +41,43 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         Log.d(TAG,"onCreate")
 
-        //======================================================
-        //kittinunf
-        kitti("google.com")
-        //kittinunf
-        //======================================================
-
-        //======================================================
-        //firebase
-        mAuth = FirebaseAuth.getInstance();
-
-        Log.d(TAG,"sebelum mAuth")
-        //createUser("ekky@yuktanding.id","sabuncolek")
-
-        //firebase
-        // ======================================================
-
         setContentView(R.layout.activity_login)
         Log.d(TAG,"setelah layout")
 
-        uil = findViewById(R.id.user_input_layout_log) as TextInputLayout
-        uie = findViewById(R.id.user_input_edit_log) as TextInputEditText
-        //ib = findViewById(R.id.image_background_log) as ImageView
-        btnSignin = findViewById(R.id.btn_Login) as Button
+        //uil = findViewById(R.id.user_input_layout_log) as TextInputLayout
+        //uie = findViewById(R.id.user_input_edit_log) as TextInputEditText
+        ib = findViewById(R.id.image_background_log) as ImageView
+        link = findViewById(R.id.txt_tanya) as TextView
+        val intent = Intent(this,SignUp::class.java)
+
+        //var spann = SpannableString(getText(R.id.txt_tanya))
+
+        val ss = SpannableString("Belum punya akun? Sign Up")
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View?) {
+                //intent
+                Log.d(TAG,"diklik")
+                startActivity(intent)
+            }
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = false
+            }
+        }
+        ss.setSpan(clickableSpan, 18, 25, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        link!!.setText(ss)
+        link!!.movementMethod = LinkMovementMethod.getInstance()
+
+        //btnSignin = findViewById(R.id.btn_Login) as Button
         //btnSignGoogle = findViewById(R.id.btn_Google) as Button
+
+        //kitti("google.com")
+        backgrnd()
+
+        mAuth = FirebaseAuth.getInstance();
+        //createUser("ekky@yuktanding.id","sabuncolek")
+
     }
 
     public override fun onStart() {
@@ -64,33 +87,10 @@ class Login : AppCompatActivity() {
         Log.d(TAG,"onStart"+currentUser)
     }
 
-    private fun createUser(uEmail:String, uPass:String) {
-
-        mAuth?.createUserWithEmailAndPassword(uEmail, uPass)
-                ?.addOnCompleteListener(this, object : OnCompleteListener<AuthResult> {
-                    override fun onComplete(task: Task<AuthResult>) {
-                        Log.d(TAG,"dalem onClomplete")
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success")
-                            val user = mAuth?.getCurrentUser()
-//                            updateUI(user)
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException())
-//                            updateUI(null)
-                        }
-
-                        // ...
-                    }
-                })
-
-    }
-
     fun backgrnd() {
         Log.d(TAG, "sebelum Picasso")
         Picasso.with(this)
-                .load("https://yuktanding.id/img/sign.jpg") //Created by Pressfoto - Freepik.com
+                .load("https://yuktanding.id/img/lap.jpg")
                 .placeholder(R.drawable.lap1)
                 .error(R.drawable.lap1)
                 .into(ib)
@@ -98,8 +98,6 @@ class Login : AppCompatActivity() {
     }
 
     fun kitti(URL: String) {
-        //======================================================
-        //kittinunf
         Fuel.get(URL).response { request, response, result ->
             println(request)
             println(response)
@@ -109,8 +107,6 @@ class Login : AppCompatActivity() {
                 println(bytes)
             }
         }
-        //kittinunf
-        //======================================================
     }
 }
 
