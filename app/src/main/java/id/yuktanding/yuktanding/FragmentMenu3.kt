298c316c7135b6c29a87487588f17b6cd91d59ee
Akise17 <ahmad.fauzan1603@gmail.com>
2017-11-17 17:23:28 +0700

    Test

--- app/src/main/java/id/yuktanding/yuktanding/FragmentMenu3.kt
+++ app/src/main/java/id/yuktanding/yuktanding/FragmentMenu3.kt
@@ -37,30 +37,30 @@ class FragmentMenu3 : Fragment() {
         user = FirebaseAuth.getInstance().currentUser!!
         if (user != null) userUID = user.uid
 
-        myRef = databaseTim.getReference().child(userUID)
+//        myRef = databaseTim.getReference().child(userUID)
 
-        myRef.addChildEventListener(object : ChildEventListener{
-            override fun onCancelled(p0: DatabaseError?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onChildMoved(p0: DataSnapshot?, p1: String?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onChildChanged(p0: DataSnapshot?, p1: String?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onChildAdded(p0: DataSnapshot?, p1: String?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onChildRemoved(p0: DataSnapshot?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-        })
+//        myRef.addChildEventListener(object : ChildEventListener{
+//            override fun onCancelled(p0: DatabaseError?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//
+//            override fun onChildMoved(p0: DataSnapshot?, p1: String?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//
+//            override fun onChildChanged(p0: DataSnapshot?, p1: String?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//
+//            override fun onChildAdded(p0: DataSnapshot?, p1: String?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//
+//            override fun onChildRemoved(p0: DataSnapshot?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//
+//        })
         Log.d(TAG, "onCreateView")
 
         timArrayList = ArrayList()
