624cc45043b15d636de3a970b19d8aeccd31cfac
ekhiw <ekkyhiw@gmail.com>
2017-11-17 16:57:36 +0700

    hackathon

--- app/src/main/java/id/yuktanding/yuktanding/ActivityMain.kt
+++ app/src/main/java/id/yuktanding/yuktanding/ActivityMain.kt
@@ -14,9 +14,6 @@ import android.os.Bundle
 import android.os.Handler
 import android.support.annotation.RequiresApi
 import android.support.design.widget.BottomNavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v4.widget.DrawerLayout
-import android.support.v7.app.ActionBarDrawerToggle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
